03ac173693d77d30831a50128ac4a5e440a13e93
jngermon <jn.germon@gmail.com>
2015-08-26 23:12:06 +0200

    Add generation of ssh-key

--- assets/install-git.sh
+++ assets/install-git.sh
@@ -3,17 +3,41 @@
 echo "Installation of git"
 
 if [ $(which git) ]; then
-	echo "Git is already installed"
-	exit
+    echo "Git is already installed"
+else
+    sudo apt-get update
+    sudo apt-get install git-core
 fi
-
-sudo apt-get update
-sudo apt-get install git-core
 
 GITCONFIG="$(pwd)/config/.gitconfig"
 
 if [ ! -f ~/.gitconfig ] || [ ! -h ~/.gitconfig ]; then
-	echo "Create symbolik link form ~/.gitconfig to $GITCONFIG"
+    echo "Create symbolik link form ~/.gitconfig to $GITCONFIG"
 
     ln --symbolic --force $GITCONFIG ~/.gitconfig
+fi
+
+SSH_FILE=~/.ssh/id_rsb
+
+if [ ! -e $SSH_FILE ]; then
+    read -p "The file $SSH_FILE doesn't exist, do you want to create one ? (Y/n) : " SSH_OK
+    case "$SSH_OK" in
+        y|Y ) SSH_OK="y";;
+        "" ) SSH_OK="y";;
+        * ) SSH_OK="n";;
+    esac
+
+    if [ "$SSH_OK" = "y" ]; then
+        SSH_EMAIL=""
+        while [ ! $SSH_EMAIL ];
+        do
+            read -p "Your email for ssh key : " SSH_EMAIL
+        done
+
+        ssh-keygen -t rsa -b 4096 -C "$SSH_EMAIL"
+
+        eval "$(ssh-agent -s)"
+
+        ssh-add $SSH_FILE
+    fi
 fi
