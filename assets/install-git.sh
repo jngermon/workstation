b90a82492fa5388f1df8bd0fb40008c5e2024b5f
jngermon <jn.germon@gmail.com>
2015-08-26 22:14:23 +0200

    Add link to config/.gitconfig

--- assets/install-git.sh
+++ assets/install-git.sh
@@ -9,3 +9,11 @@ fi
 
 sudo apt-get update
 sudo apt-get install git-core
+
+GITCONFIG="$(pwd)/config/.gitconfig"
+
+if [ ! -f ~/.gitconfig ] || [ ! -h ~/.gitconfig ]; then
+	echo "Create symbolik link form ~/.gitconfig to $GITCONFIG"
+
+    ln --symbolic --force $GITCONFIG ~/.gitconfig
+fi
